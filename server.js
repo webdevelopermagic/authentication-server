9f9780b8003280ee20f38d13884eb3ab45c5df97
webdevelopermagic <152077263+webdevelopermagic@users.noreply.github.com>
2024-01-12 09:54:45 +0530

    added files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler.js");
+const connectDb = require("./config/dbConnection.js");
+const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes.js"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log('server running on port'  +  port);
+
+});
